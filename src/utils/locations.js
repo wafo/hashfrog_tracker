31699a50bb9f1a86f641b5cd9d6935e95106c9bf
Jonathan Tan <jontan_@hotmail.com>
2023-07-20 15:20:33 -0400

    Remove duplicate code

--- src/utils/locations.js
+++ src/utils/locations.js
@@ -149,11 +149,6 @@ class Locations {
       return false;
     }
 
-    // Bombchu Bowling 3rd and 4th prizes (must be checked before Bombchu vanilla items!)
-    else if (_.includes(["Market Bombchu Bowling Bombchus", "Market Bombchu Bowling Bomb"], location.locationName)) {
-      return false;
-    }
-
     // Bombchus
     else if (_.includes(["Bombchus", "Bombchus (5)", "Bombchus (10)", "Bombchus (20)"], location.vanillaItem)) {
       return (
